f16b85ad7d3f0a60eebaba436a980582dd941ed9
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-06 17:55:04 +0530

    landding page created

--- edutrack/settings.py
+++ edutrack/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "accounts",
     "students",
     "django.contrib.admin",
     "django.contrib.auth",
@@ -116,3 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = "static/"
+
+STATICFILES_DIRS=[
+    BASE_DIR /"static"
+]
