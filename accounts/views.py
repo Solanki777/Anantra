71b8416e598789a4c5e3af0d5c93c8af709cae00
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-06 23:06:25 +0530

    login page completed

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
+
 
 def register_view(request):
 
@@ -14,3 +17,30 @@ def register_view(request):
         form=RegisterForm()
 
     return render(request, "accounts/register.html",{"form":form})
+
+
+def login_view(request):
+
+    if request.method=="POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(
+
+            request,
+            username=username,
+            password=password
+        )
+
+        if user is not None:
+            login(request, user)
+
+            return redirect("dashboard")
+        
+        else:
+            messages.error(
+                request,
+                "Invalid Username password"
+            )
+        
+    return render(request, "accounts/login.html")
