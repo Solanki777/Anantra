e99064336c0e70d9b231ec333e122feb0974659f
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-08 21:25:09 +0530

    migrations for students are done

--- students/models.py
+++ students/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    mobile = models.CharField(max_length=15)
+    course = models.CharField(max_length=100)
+    department = models.CharField(max_length=100)
+    admission_date = models.DateField()
+
+    def __str__(self):
+        return self.name
