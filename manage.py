0fec2daab8046920ef845ac160d018b7f5ed64e2
Solanki777 <solankimaheshkhash7@gmail.com>
2026-07-23 15:37:35 +0530

    good morning Yo !

--- manage.py
+++ manage.py
@@ -7,7 +7,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "edutrack.settings")
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Anantra.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
