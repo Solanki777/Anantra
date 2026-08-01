4c25bfe51f23fa24a9f3122e97da9f06bf373c4a
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-01 22:50:00 +0530

    forget password ?

--- Anantra/urls.py
+++ Anantra/urls.py
@@ -19,12 +19,40 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",include("students.urls")),
     path("",include("accounts.urls")),
-    path("superadmin/",include("superadmin.urls"))
+    path("superadmin/",include("superadmin.urls")),
+    path( "password-reset/",auth_views.PasswordResetView.as_view(
+        template_name = "accounts/password_reset.html"
+    )),
+    path(
+        "password-reset/done",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name = "accounts/password_reset_done.html"
+        ),
+        name = "password_reset_done",
+    ),
+    path(
+        "reset/<uid64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name = "accounts/password_reset_confirm.html"
+        ),
+        name = "password_reset_confirm"
+    ),
+
+    path(
+        "reset-complete/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name = "accounts/password_reset_complete.html"
+        ),
+        name = "password_reset_complete"
+    )
+    
+
    
 
 
