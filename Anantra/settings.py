c0ec0e9e01fabc1d484542c184b2c31fa4df28c9
Solanki777 <solankimaheshkhash7@gmail.com>
2026-08-10 16:26:52 +0530

    bravo gmail configuration done

--- Anantra/settings.py
+++ Anantra/settings.py
@@ -136,12 +136,25 @@ MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media"
 
 # Email Configuration
-# Email Configuration
+
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
-EMAIL_HOST = config("EMAIL_HOST")
-EMAIL_PORT = config("EMAIL_PORT", cast=int)
-EMAIL_USE_TLS = config("EMAIL_USE_TLS", default=True, cast=bool)
+EMAIL_HOST = config(
+    "EMAIL_HOST",
+    default="smtp-relay.brevo.com"
+)
+
+EMAIL_PORT = config(
+    "EMAIL_PORT",
+    default=587,
+    cast=int
+)
+
+EMAIL_USE_TLS = config(
+    "EMAIL_USE_TLS",
+    default=True,
+    cast=bool
+)
 
 EMAIL_HOST_USER = config("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = config("EMAIL_HOST_PASSWORD")
@@ -150,3 +163,5 @@ DEFAULT_FROM_EMAIL = config(
     "DEFAULT_FROM_EMAIL",
     default=EMAIL_HOST_USER
 )
+
+EMAIL_TIMEOUT = 30
